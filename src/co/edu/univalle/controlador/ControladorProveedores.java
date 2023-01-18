446ea2f823beb0cc9f22592dbaaeecf5d6b0c9d1
Christian Vargas <vargas.christian@correounivalle.edu.co>
2023-01-18 00:37:02 -0500

    Limpié el código y estructuré la primera parte de la integración

--- src/co/edu/univalle/controlador/ControladorProveedores.java
+++ src/co/edu/univalle/controlador/ControladorProveedores.java
@@ -1,13 +1,39 @@
+/*
+    Archivo: ControladorProveedores.java
+    Fundamentos de Programación Orientada a Eventos - 750014C Grupo 01
+    Proyecto 4 - Supermercado Universidad del Valle
+
+    Autores: 
+    Juan Camilo Narvaez Tascon - juan.narvaez.tascon@correounivalle.edu.co - 2140112-3743
+    Christian David Vargas Gutiérrez - vargas.christian@correounivalle.edu.co - 2179172-3743
+
+    Profesor:
+    Ing. M.Sc. Luis Yovany Romo Portilla
+
+    Licencia: GNU-GPL
+*/
+
+/**
+    CLASE: ControladorProveedores
+
+    INTENCIÓN: Esta es la clase controladora de la categoría proveedores.
+    Esta clase se encarga orientar los eventos relacionados con los proveedores del supermercado.
+    
+    RELACIONES:
+*/
+
+
+
 package co.edu.univalle.controlador;
 
-import javax.swing.JComboBox;
-import javax.swing.JTextField;
+import co.edu.univalle.vista.*;
+import co.edu.univalle.modelo.*;
+import javax.swing.*;
 
-import co.edu.univalle.vista.VentanaInicio;
 
 public class ControladorProveedores {
-    private static String[] labelProveedores = {"NIT ", "Nombre ", "Correo ", "Teléfono "};
-    String encabezadoProveedores[] = {"NIT", "Nombre completo", "Correo", "Teléfono", "Compras registradas"};
+    private static String[] labelProveedores = {"NIT ", "Nombre del proveedor ", "Correo ", "Teléfono "};
+    private static String encabezadoProveedores[] = {"NIT", "Nombre del proveedor", "Correo", "Teléfono", "Transacciones registradas"};
     
     public static void pintar(VentanaInicio ventanaInicio) {
         ventanaInicio.getLabelTexto()[0].setText(labelProveedores[0]);
@@ -24,7 +50,6 @@ public class ControladorProveedores {
 
     }
 
-
     public static void limpiar(VentanaInicio ventanaInicio) {
         ventanaInicio.getFieldIdProveedor().setText("");
         ventanaInicio.getFieldIdProveedor().setEditable(true);
@@ -32,4 +57,24 @@ public class ControladorProveedores {
         ventanaInicio.getFieldTelefonoProveedor().setText("");
         ventanaInicio.getFieldCorreoProveedor().setText("");
     }
+
+    public static String[] getEncabezadoProveedores() {
+        return encabezadoProveedores;
+    }
+
+    public static boolean revisarFieldsProveedores(VentanaInicio ventanaInicio){
+
+        return true;
+    }
+
+    public static Proveedor crearProveedor(VentanaInicio ventanaInicio){
+
+        return null;
+    }
+
+    public static boolean revisarIDProveedor(VentanaInicio ventanaInicio){
+
+        return true;
+    }
+
 }
---
+++ src/co/edu/univalle/controlador/ControladorVentas.java
@@ -0,0 +1,84 @@
+/*
+    Archivo: ControladorVentas.java
+    Fundamentos de Programación Orientada a Eventos - 750014C Grupo 01
+    Proyecto 4 - Supermercado Universidad del Valle
+
+    Autores: 
+    Juan Camilo Narvaez Tascon - juan.narvaez.tascon@correounivalle.edu.co - 2140112-3743
+    Christian David Vargas Gutiérrez - vargas.christian@correounivalle.edu.co - 2179172-3743
+
+    Profesor:
+    Ing. M.Sc. Luis Yovany Romo Portilla
+
+    Licencia: GNU-GPL
+*/
+
+/**
+    CLASE: ControladorVentas
+
+    INTENCIÓN: Esta es la clase controladora de la categoría ventas
+    Esta clase se encarga orientar los eventos relacionados con las ventas del supermercado.
+    
+    RELACIONES:
+*/
+
+package co.edu.univalle.controlador;
+
+import co.edu.univalle.modelo.*;
+import co.edu.univalle.vista.*;
+
+public class ControladorVentas {
+    
+    private static String[] labelVenta = {"ID de la venta ", "Fecha de la venta ", "Cédula del cliente ", "Nombre del cliente ", "Valor de la factura ", "Lista de productos "};
+    private static String encabezadoVenta[] = {"ID de la venta", "Fecha de la venta", "Cédula del cliente", "Nombre del cliente", "Número de productos", "Valor de la factura"};
+    
+    public static void pintar(VentanaInicio ventanaInicio) {
+        ventanaInicio.getLabelTexto()[0].setText(labelVenta[0]);
+        ventanaInicio.getContenedorTexto()[0].add(ventanaInicio.getFieldIdVenta());
+
+        ventanaInicio.getLabelTexto()[1].setText(labelVenta[1]);
+        ventanaInicio.getContenedorTexto()[1].add(ventanaInicio.getFieldFechaVenta());
+        
+        ventanaInicio.getLabelTexto()[2].setText(labelVenta[2]);
+        ventanaInicio.getContenedorTexto()[2].add(ventanaInicio.getFieldCedulaClienteVenta());
+
+        ventanaInicio.getLabelTexto()[3].setText(labelVenta[3]);
+        ventanaInicio.getContenedorTexto()[3].add(ventanaInicio.getFieldNombresClienteVenta());
+
+        ventanaInicio.getLabelTexto()[4].setText(labelVenta[4]);
+        ventanaInicio.getContenedorTexto()[4].add(ventanaInicio.getFieldCostoVenta());
+
+        ventanaInicio.getLabelTexto()[5].setText(labelVenta[5]);
+        ventanaInicio.getContenedorTexto()[5].add(ventanaInicio.getbuttonListaProductosVenta());
+
+    }
+
+    public static void limpiar(VentanaInicio ventanaInicio) {
+        ventanaInicio.getFieldIdVenta().setText("");
+        ventanaInicio.getFieldFechaVenta().setText("");
+        ventanaInicio.getFieldCedulaClienteVenta().setText("");
+        ventanaInicio.getFieldNombresClienteVenta().setText("");
+        ventanaInicio.getFieldCostoVenta().setText("");
+    }
+
+
+    public static String[] getEncabezadoVenta() {
+        return encabezadoVenta;
+    }
+    
+    public static boolean revisarFieldsVentas(VentanaInicio ventanaInicio){
+
+        return true;
+    }
+
+    public static Venta crearVenta(VentanaInicio ventanaInicio){
+
+        return null;
+    }
+
+    public static boolean revisarIDVenta(VentanaInicio ventanaInicio){
+
+        return true;
+    }
+
+}
